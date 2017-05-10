9f6f96a170fb25dc850b6d8a43296a6b6a0e4074
mdavila04 <mdavila@umail.ucsb.edu>
2017-05-09 22:39:44 -0700

    Add files via upload

--- poly2.cxx
+++ poly2.cxx
@@ -8,8 +8,9 @@ namespace main_savitch_5
             // CONSTRUCTORS and DESTRUCTOR
         polynomial(double c = 0.0, unsigned int exponent = 0)
 	{
+	  head_ptr
 	  if(c = 0.0)
-	    current_degree = exponent;
+	    current_degree = 0;
 	    current_degree = exponent;
 	}
         polynomial(const polynomial& source)
@@ -51,6 +52,33 @@ namespace main_savitch_5
         unsigned int current_degree;    // Current degree of the polynomial
         // A private member function to aid the other functions:
         void set_recent(unsigned int exponent) const;
+	{
+	  if(exponent == 0) //if exponent is 0 set recent to head
+	    recent_ptr = head_ptr;
+	  if(exponent >= current_degree)// if exponent greater than current degree set to tail
+	    recent_ptr = tail_ptr;
+	  if(exponent < recent_ptr->exponent) //Move recent_ptr back to match exponent
+	    {
+	      const node *cursor;
+	      for(cursor = recent_ptr;cursor != NULL; cursor = cursor ->back())
+		{
+		  if(recent_ptr->exponent != exponent)
+		    recent_ptr->back();
+		  else continue;
+		}
+	    }
+	  else//move recent foreward to match exponent
+	    {
+	      const node *cursor;
+	      for(cursor = recent_ptr; cursor != NULL; cursor = cursor->fore())
+		{
+		  if(recent_ptr->exponent != exponent)
+		    recent_ptr->fore();
+		  else continue;
+		}
+	    }
+	}
+	    
     };
     
     // NON-MEMBER BINARY OPERATORS
