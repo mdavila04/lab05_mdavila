455c941fa12aecc1ccbaddd8984bc767e5a40266
mdavila04 <mdavila@umail.ucsb.edu>
2017-05-10 21:54:21 -0700

    Add files via upload

--- poly2.cxx
+++ poly2.cxx
@@ -6,31 +6,84 @@
 namespace main_savitch_5
 {
             // CONSTRUCTORS and DESTRUCTOR
-        polynomial(double c = 0.0, unsigned int exponent = 0)
+        polynomial::polynomial(double c = 0.0, unsigned int exponent = 0)
 	{
-	  head_ptr
 	  if(c = 0.0)
-	    current_degree = 0;
+      {
+        head_ptr->set_coef(0.0)
+      head_ptr->set_exponent(0)
+      current_degree = 0;
+      }
+    else
+        head_ptr->set_exponent(exponent)
 	    current_degree = exponent;
+    if(c != 0.0)
+        head_ptr->set_coef(c)
 	}
-        polynomial(const polynomial& source)
+        polynomial::polynomial(const polynomial& source)
+        {
+            head_ptr = source.head_ptr;
+            current_degree = source.current_degree;
+            tail_ptr = source.tail_ptr;
+            for()//copy all data from source nodes to new nodes
+            {
+                head_ptr.set_coef(source.coefficient);
+                head_ptr.set_exponent(source.degree);
+            }
+        }
         ~polynomial( )
 	{
-	  delete;
+        clear();
 	}
 
         // MODIFICATION MEMBER FUNCTIONS
         polynomial& operator =(const polynomial& source)
-        void add_to_coef(double amount, unsigned int exponent)
-        void assign_coef(double coefficient, unsigned int exponent)
-        void clear( )
+        {
+
+        }
+        void polynomial::add_to_coef(double amount, unsigned int exponent)
+        {
+            set_recent(exponent);
+            recent_ptr->set_coef()
+        }
+        void polynomial::assign_coef(double coefficient, unsigned int exponent)
+        {
+            set_recent(exponent);
+            if(exponent>current_degree && coefficient==0.0)
+            {
+                return;
+            }
+            if(recent_ptr == NULL)//no node then new node and care for tail_ptr
+            {
+                new polynode new_node;
+                new_node->set_coef(coefficient);
+                new_node->set_exponent(exponent);
+            }
+            if(exponent = 0 || coefficient != 0.0)
+            {
+                recent_ptr->set_coef(coefficient);
+            }
+            recent_ptr->set_coef(coefficient);
+            recent_ptr->set_exponent(exponent);
+        }
+        void polynomial::clear( )
+        {
+            polynode *n = head_ptr;
+        while(n)
+        {
+            Node *del = n;
+            n->fore();
+            delete del;
+        }
+
+        }
         
         // CONSTANT MEMBER FUNCTIONS
-        double coefficient(unsigned int exponent) const
-        unsigned int degree( ) const { return current_degree; }
-        polynomial derivative( ) const
-        double eval(double x) const
-        void find_root(
+        double polynomial::coefficient(unsigned int exponent) const
+        unsigned int polynomial::degree( ) const { return current_degree; }
+        polynomial polynomial::derivative( ) const
+        double polynomial::eval(double x) const
+        void polynomial::find_root(
             double& answer,
             bool& success,
             unsigned int& iterations,
@@ -39,8 +92,8 @@ namespace main_savitch_5
             double epsilon = 1e-8
             )
             const
-        unsigned int next_term(unsigned int e) const
-        unsigned int previous_term(unsigned int e) const
+        unsigned int polynomial::next_term(unsigned int e) const
+        unsigned int polynomial::previous_term(unsigned int e) const
         
         // CONSTANT OPERATORS
         double operator( ) (double x) const { return eval(x); }
@@ -59,16 +112,17 @@ namespace main_savitch_5
 	    recent_ptr = tail_ptr;
 	  if(exponent < recent_ptr->exponent) //Move recent_ptr back to match exponent
 	    {
-	      const node *cursor;
-	      for(cursor = recent_ptr;cursor != NULL; cursor = cursor ->back())
-		{
-		  if(recent_ptr->exponent != exponent)
-		    recent_ptr->back();
-		  else continue;
-		}
+
+        	      for(;recent_ptr != NULL && recent_pointer->exponent != exponent; recent_pointer = recent_pointer ->back());
+                    if(recent_pointer == NULL)
+                    {
+                        recent_pointer = head_ptr;
+                    }
 	    }
 	  else//move recent foreward to match exponent
 	    {
+        for(;recent_ptr != NULL && recent_pointer->exponent != exponent; recent_pointer = recent_pointer ->fore());//moves forward to tail
+            if(recent_pointer == tail_ptr)
 	      const node *cursor;
 	      for(cursor = recent_ptr; cursor != NULL; cursor = cursor->fore())
 		{
